704e1706a2d78824180c2781cb52269441cedf88
Graham Tremper <gtremper@gmail.com>
2013-07-18 18:20:43 -0700

    changing shit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,57 +1,77 @@
 'use strict';
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
-    var people = []
-    people.push({name:'Graham', items:[1.99,4.33]});
-    people.push({name:'Becca', items:[2.33,1.99,4.33]});
-    people.push({name:'Oliver', items:[1.11,4.33]});
-    people.push({name:'Max', items:[1.99,4.33, 23.34]});
-    people.push({name:'Jon', items:[1.99]});
-    $scope.people = people;
+  /* Hardcoded values for test */
+  function transaction(owner,split,amount){
+    this.owner = owner;
+    this.split = split;
+    this.split.push(owner);
+    this.amount = amount;
+    this.date = new Date();
+  }
 
-    $scope.opts = {
-      backdropFade: true,
-      dialogFade:true
-    };
+  $scope.transactions = [];
+  $scope.transactions.push(new transaction('Graham',['Max','Oli'],12.99));
+  $scope.transactions.push(new transaction('Max',['Graham','Oli'],3.99));
+  $scope.transactions.push(new transaction('Oli',['Graham','Max'],5.99));
+  $scope.owners = ['Graham','Max','Oli'];
 
-    $scope.total = function(person){
-      var amt = 0;
-      angular.forEach(person.items,function(item){
-        amt += item;
-      })
-      return amt;
-    }
-
-    $scope.allTotal = function(){
-      var amt = 0;
-      angular.forEach(people,function(person){
-        amt += $scope.total(person);
-      })
-      return amt;
-    }
-
-    $scope.average = function(){
-      return $scope.allTotal() / people.length;
-    }
-
-    $scope.getBalance =function(person){
-      return $scope.total(person) - $scope.average();
-    }
-
-    $scope.addItem = function(person){
-      if(!person.input){
-        return;
+  $scope.total = function(person){
+    var amt = 0;
+    angular.forEach($scope.transactions,function(trans){
+      if (trans.owner === person){
+        amt += trans.amount;
       }
-      person.items.push(person.input);
-      person.input = "";
-    }
+    })
+    return amt;
+  }
 
-    $scope.open = function(person){
-      $scope.newItemModal = true;
-      $scope.currentUser = person;
-    }
+  $scope.allTotal = function(){
+    var amt = 0;
+    angular.forEach($scope.transactions,function(trans){
+      amt += trans.amount;
+    })
+    return amt;
+  }
 
-    $scope.close = function () {
-      $scope.newItemModal = false;
-    };
-  }]);
+  $scope.getItems = function(person){
+    var items = []
+    angular.forEach($scope.transactions,function(trans){
+      if (trans.owner === person){
+        items.push(trans);
+      }
+    });
+    return items;
+  }
+
+  $scope.average = function(){
+    return $scope.allTotal() / $scope.owners.length;
+  }
+
+  $scope.getBalance =function(person){
+    return $scope.total(person) - $scope.average();
+  }
+
+  $scope.addItem = function(person){
+    if(!person.input){
+      return;
+    }
+    transaction.items.push(person.input);
+    person.input = "";
+  }
+
+  /* Modal stuff */
+  $scope.opts = {
+    backdropFade: true,
+    dialogFade:true
+  };
+
+  $scope.open = function(person){
+    $scope.newItemModal = true;
+    $scope.currentUser = person;
+  }
+
+  $scope.close = function () {
+    $scope.newItemModal = false;
+  };
+}]);
